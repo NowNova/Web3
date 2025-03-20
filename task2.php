5b5760ce37dde1718d6810518b7a5133f4a877d7
NowNova <146940511+NowNova@users.noreply.github.com>
2025-03-20 16:57:10 +0200

    аштфд

--- task2.php
+++ task2.php
@@ -11,7 +11,7 @@ echo "\nЧ исло с плавающей запятой: $num_float\n";
 
 
 echo "\n Число ", 6 + 3 + 3, " нигде не встречалось в коде \n";
-$last_Month = 500;
-$current_Month = 350;
+$last_Month = 900;
+$this_Month = 300;
 
-echo "\n Насколько больше я потратил в прошлом месяце, чем в этом месяце? ", $last_Month - $current_Month;
+echo "\n Насколько больше я потратил в прошлом месяце, чем в этом месяце? ", $last_Month - $this_Month;
